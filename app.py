26cb3b4485724fd19c7b9ea0f3122095721b9dd7
Rakesh V Shetty <rakeshvshetty.ai22@rvce.edu.in>
2025-07-22 01:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapapi.errors import HttpError
 from googleapiclient.errors import HttpError
 import uuid
 import csv
